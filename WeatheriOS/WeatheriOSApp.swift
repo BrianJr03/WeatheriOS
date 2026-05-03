03b017c03250db8627de06586918b530fd596379
Brian Jr <thaballa79@gmail.com>
2026-05-03 14:25:13 -0400

    Initial Commit

---
+++ WeatheriOS/WeatheriOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatheriOSApp.swift
+//  WeatheriOS
+//
+//  Created by Brian Walker Jr on 5/3/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatheriOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
